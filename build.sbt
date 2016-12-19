fd70060b236895171c6c30ab576d97a98c323592
Jan Schulte <jan@janschulte.com>
2016-12-19 20:57:19 +0100

    Initial commit

---
+++ build.sbt
@@ -0,0 +1,7 @@
+
+organization := "com.janschulte.course"
+scalaVersion := "2.11.8"
+    
+name := "beginner"
+
+libraryDependencies ++= Seq("org.specs2" %% "specs2-core" % "3.8.5" % "test")
---
+++ src/test/scala/example/HelloSpec.scala
@@ -0,0 +1,14 @@
+package example
+
+import org.specs2.mutable.Spec
+
+class HelloSpec extends Spec {
+
+  "The Hello object" should {
+
+    "say hello" in {
+      Hello.greeting must be_==("hello")
+    }
+
+  }
+}
